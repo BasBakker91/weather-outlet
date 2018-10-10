2ba11b846b09d8d31f73888517ced1b29f17a9ce
BasBakker91 <basbakker1991@gmail.com>
2018-10-10 23:39:12 +0200

    Added Todo Views

--- WeatherOutlet.Client/Services/AppState.cs
+++ WeatherOutlet.Client/Services/AppState.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using WeatherOutlet.Shared;
 using WeatherOutlet.Shared.Places;
+using WeatherOutlet.Shared.Models;
 
 namespace WeatherOutlet.Client.Services
 {
@@ -16,11 +17,15 @@ namespace WeatherOutlet.Client.Services
         private readonly IToaster toaster;
 
         public List<string> SearchHistory { get; set; }
+
         public PlaceData PlaceData { get; set; }
+        public List<TodoItem> Todos { get; set; }
+
         public bool SearchInProgress { get; private set; }
         public HttpClient http { get; }
 
         public event Action OnAppStateChanged;
+        public delegate Task OnMarkTodoAsCompletedHandler(TodoItem todoItem);
 
         public AppState(LocalStorage localStorage, IToaster toaster, HttpClient httpClient)
         {
@@ -28,6 +33,26 @@ namespace WeatherOutlet.Client.Services
             this.toaster = toaster;
             http = httpClient;
             GetSearchHistory();
+        }
+
+        public async Task LoadTodos()
+        {
+            SearchInProgress = true;
+            NotifyStateChanged();
+
+            Todos = await http.GetJsonAsync<List<TodoItem>>($"/api/todos");
+
+            SearchInProgress = false;
+            NotifyStateChanged();
+        }
+
+        public async Task OnMarkTodoAsCompleted(TodoItem todoItem)
+        {
+            todoItem.IsCompleted = true;
+            todoItem.CompletedAt = DateTime.Now;
+
+            await http.PutJsonAsync<TodoItem>($"/api/todos/{todoItem.Id}", todoItem);
+            await LoadTodos();
         }
 
         public async Task Search(SearchCriteria searchCriteria)
